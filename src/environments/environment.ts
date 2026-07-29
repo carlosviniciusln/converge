5ac05c355b5ec419eac9c2a61b76ad7c2217b054
Carlos Vinicius <carlosvinicius.ln@gmail.com>
2026-07-28 21:29:26 -0300

    Add environment configuration for production

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  end_point: '__BACK_API_URL__',
+  KEYCLOAK_URL: 'https://iam.converge.com/',
+  KEYCLOAK_REALM: 'internet',
+  KEYCLOAK_CLIENT_ID: 'cli-web-cvg'
+};
