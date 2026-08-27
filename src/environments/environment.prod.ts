7fe613e0dd8a255f53c443304fb0eca46dbe522b
Carlos Vinicius <carlosvinicius.ln@gmail.com>
2026-08-27 19:57:50 -0300

    fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  useLocalManagementData: false,
+  useLocalManagementData: true,
   //end_point: 'http://api.sigvc.hmp.caixa/api',
   //end_point: 'http://api.sigvc.caixa/api',
   //end_point: 'https://localhost:5001/api',
