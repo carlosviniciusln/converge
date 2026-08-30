9536805e69d55de41661bad2e028052ab54a1274
Carlos Vinicius <carlosvinicius.ln@gmail.com>
2026-08-30 12:45:44 -0300

    fix menu

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,7 +8,6 @@ import { SidenavService } from '../services/sidenav.service';
 })
 export class PagesComponent implements OnInit {
   public sidebarColor: string = "red";
-  public sidebarHovered = false;
 
   constructor(public sidenav: SidenavService) {}
   changeSidebarColor(color: string){
